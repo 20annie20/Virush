fc96c7c0916dff6799f2282d3ccd5bdb477dab75
Annie-2020 <41429556+20annie20@users.noreply.github.com>
2020-05-25 13:56:16 +0200

    Add files via upload

---
+++ Creature.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    public GameObject coinPrefab;
+    [SerializeField]
+    protected int coins;
+    [SerializeField]
+    protected int health;
+    protected bool isAlive;
+    protected SpriteRenderer sprite;
+
+    public int Health
+    {
+        get => health;
+        set => health = value;
+    }
+    public int Coins
+    {
+        get => coins;
+        set => coins = value;
+    }
+
+    protected void ChangeColorWhenDamaged()
+    {
+        StartCoroutine(ChangeColorRoutine());
+    }
+
+    IEnumerator ChangeColorRoutine()
+    {
+        Color red = new Color(1, -1f, -1f, 1);
+        sprite.color = red;
+        yield return new WaitForSeconds(0.3f);
+        sprite.color = new Color(1, 1, 1, 1);
+    }
+}
---
+++ SaveSystem.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SavePlayer(Player player)
+    {
+        Debug.Log("System: saving");
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player)
+        {
+            coins = player.Coins,
+            level = player.Level,
+            maxHealth = player.MaxHealth,
+            speedRate = player.SpeedRate,
+            damageValue = player.DamageValue
+        };
+
+        Debug.Log("Coins in data: "+ data.coins);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+
+        Debug.Log("what is in the saved data: " + "maxHealth " + data.maxHealth
+            + ", level " + data.level + ", coins " + data.coins +
+            ", damageValue" + data.damageValue + ", speedRate" + data.speedRate
+            ); 
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+            Debug.Log("what is in the loaded data: " + "maxHealth " + data.maxHealth
+            + ", level " + data.level + ", coins " + data.coins +
+            ", damageValue" + data.damageValue + ", speedRate" + data.speedRate);
+            return data;
+
+        } else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
