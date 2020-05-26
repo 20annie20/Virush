4317fe81df18c12838908b28d4065003cb0d5941
Annie-2020 <41429556+20annie20@users.noreply.github.com>
2020-05-26 21:54:54 +0200

    testy

---
+++ Tests/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BulletScript
+    {
+        [Test]
+        public void BulletInstantiates()
+        {
+            Assert.IsNotNull(Object.Instantiate(Resources.Load("prefabs/bullet"), Vector3.zero, Quaternion.identity));
+        }
+        
+        [UnityTest]
+        public IEnumerator BulletDoesNotMoveUpAndDown()
+        {
+            GameObject bullet = (GameObject)Object.Instantiate(Resources.Load("prefabs/bullet"), Vector3.zero, Quaternion.identity);
+            float initialPosY = bullet.transform.position.y;
+            yield return new WaitForSeconds(0.5f);
+            float newPos = bullet.transform.position.y;
+            if(newPos < 0.1f)
+            {
+                newPos = 0;
+            }
+            Assert.AreEqual(newPos, initialPosY);
+            Object.Destroy(bullet.gameObject);
+        }
+    }
+}
